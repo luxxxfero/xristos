4803b000f89db6407f66057ff30441353e2f05b9
luxxxfero <aleks.ferolux@gmail.com>
2022-08-07 15:38:38 +0500

    add metrika

--- src/layouts/main-layout.tsx
+++ src/layouts/main-layout.tsx
@@ -1,6 +1,11 @@
 import React, {DetailedHTMLProps, HTMLAttributes} from "react";
 import styles from "./main-layout.module.css";
 import Link from "next/link";
+import OKIcon from "../components/icons/ok";
+import WhatsappIcon from "../components/icons/whatsapp";
+import VKIcon from "../components/icons/vk";
+import TelegramIcon from "../components/icons/telegram";
+import cn from "classnames";
 
 interface MainLayout extends DetailedHTMLProps<HTMLAttributes<HTMLElement>, HTMLElement> {
   title: string,
@@ -23,6 +28,32 @@ const MainLayout: React.FC<MainLayout> = ({children, title, desc}) => {
           <p className={styles.contentDesc}>{desc}</p>
           <div className={styles.contentWrap}>
             {children}
+          </div>
+          <div className={styles.mainFooter}>
+            <div className={styles.share}>
+              <h4 className={styles.shareTitle}>
+                Поделиться
+              </h4>
+              <a target="_blank" href="https://connect.ok.ru/offer?url=https://xristos.ru/&title=Благая весть - Христос.ру&utm_source=share2" className={cn(styles.shareLink)}>
+                <OKIcon className={cn(styles.shareIcon)}/>
+              </a>
+              <a target="_blank" href="https://api.whatsapp.com/send?text=Благая весть - Христос.ру https://xristos.ru/&utm_source=share2" className={cn(styles.shareLink)}>
+                <WhatsappIcon className={cn(styles.shareIcon)}/>
+              </a>
+              <a target="_blank" href="https://vk.com/share.php?url=https://xristos.ru/&title=Благая весть - Христос.ру&utm_source=share2" className={cn(styles.shareLink)}>
+                <VKIcon className={cn(styles.shareIcon)}/>
+              </a>
+              <a target="_blank" href="https://t.me/share/url?url=https://xristos.ru/&text=Благая весть - Христос.ру&utm_source=share2" className={cn(styles.shareLink)}>
+                <TelegramIcon className={cn(styles.shareIcon)}/>
+              </a>
+            </div>
+            <div>
+              <h4 className={styles.shareTitle}>
+                <Link href="/thanks">
+                  <a>Поблагодарить</a>
+                </Link>
+              </h4>
+            </div>
           </div>
         </section>
       </main>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,22 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
+import {Router} from "next/router";
+import ym, {YMInitializer} from "react-yandex-metrika";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+Router.events.on("routeChangeComplete", (url: string) => {
+  if(typeof window !== "undefined") {
+    ym("hit", url)
+  }
+})
+
+function MyApp({Component, pageProps}: AppProps) {
+  return (
+    <>
+      <YMInitializer accounts={[89869532]} options={{webvisor: false, defer: true}}/>
+      <Component {...pageProps} />
+    </>
+
+  )
 }
 
 export default MyApp
